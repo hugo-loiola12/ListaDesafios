25f20f3d5a1d5f8705a4f20471164e0373dd5e18
Hugo Loiola <hugoloiola.1@gmail.com>
2024-11-01 15:33:43 -0300

    Herança começo

---
+++ src/main/java/br/com/heranca/veiculos/Carro.java
@@ -0,0 +1,32 @@
+package br.com.heranca.veiculos;
+
+public class Carro extends Veiculo {
+
+    private int quantidadePortas;
+    private float tamanhoPortaMalas;
+
+    public Carro(String marca, String modelo, int ano, int quantidadePortas, float tamanhoPortaMalas) {
+        super();
+        this.setMarca(marca);
+        this.setModelo(modelo);
+        this.setAno(ano);
+        this.quantidadePortas = quantidadePortas;
+        this.tamanhoPortaMalas = tamanhoPortaMalas;
+    }
+
+    public int getQuantidadePortas() {
+        return quantidadePortas;
+    }
+
+    public void setQuantidadePortas(int quantidadePortas) {
+        this.quantidadePortas = quantidadePortas;
+    }
+
+    public float getTamanhoPortaMalas() {
+        return tamanhoPortaMalas;
+    }
+
+    public void setTamanhoPortaMalas(float tamanhoPortaMalas) {
+        this.tamanhoPortaMalas = tamanhoPortaMalas;
+    }
+}
---
+++ src/main/java/br/com/heranca/veiculos/Moto.java
@@ -0,0 +1,22 @@
+package br.com.heranca.veiculos;
+
+public class Moto extends Veiculo {
+
+    private int cilindradas;
+
+    public Moto(String marca, String modelo, int ano, int cilindradas) {
+        super();
+        this.setMarca(marca);
+        this.setModelo(modelo);
+        this.setAno(ano);
+        this.cilindradas = cilindradas;
+    }
+
+    public int getCilindradas() {
+        return cilindradas;
+    }
+
+    public int setCilindradas(int cilindradas) {
+        return this.cilindradas = cilindradas;
+    }
+}
